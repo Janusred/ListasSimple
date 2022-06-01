3c01b0f6ec98604b52d2155d9243f61fb027d240
Janusred <horaciok300@hotmail.com>
2022-06-01 12:54:55 -0500

    Final

--- app/src/main/java/com/example/listasimple/AdvancedListActivity.java
+++ app/src/main/java/com/example/listasimple/AdvancedListActivity.java
@@ -1,4 +1,37 @@
 package com.example.listasimple;
+import android.app.Activity;
+import android.os.Bundle;
 
-public class AdvancedListActivity {
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.wear.widget.WearableRecyclerView;
+import android.widget.LinearLayout;
+public class AdvancedListActivity  extends Activity {
+
+   WearableRecyclerView lista_avanzada;
+   AdvancedAdapter adaptador;
+   ListaEquipos listaEquipos;
+LinearLayoutManager layoutManager;
+   @Override
+    protected void onCreate(Bundle savedInstanceState){
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_advanced_list);
+
+       listaEquipos = new ListaEquipos();
+       adaptador= new AdvancedAdapter(listaEquipos);
+
+       lista_avanzada= findViewById(R.id.advanced_recyclerview);
+  lista_avanzada.setEdgeItemsCenteringEnabled(true);
+         lista_avanzada.setHasFixedSize(true);
+
+       layoutManager = new LinearLayoutManager(this);
+       lista_avanzada.setLayoutManager(layoutManager);
+       DividerItemDecoration dividerItemDecoration = new
+               DividerItemDecoration(lista_avanzada.getContext(),
+               layoutManager.getOrientation());
+       lista_avanzada.addItemDecoration(dividerItemDecoration);
+
+       lista_avanzada.setAdapter(adaptador);
+   }
+
 }
