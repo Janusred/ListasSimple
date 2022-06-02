dde810bffb4c97c7b64330f73d392aa8e9dfffc1
Janusred <horaciok300@hotmail.com>
2022-06-02 01:50:29 -0500

    Miercoles

--- app/src/main/java/com/example/listasimple/MainActivity.java
+++ app/src/main/java/com/example/listasimple/MainActivity.java
@@ -3,39 +3,34 @@ package com.example.listasimple;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.wearable.activity.WearableActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import com.example.listasimple.databindig.ActivityMainBinding;
 
-public class MainActivity extends Activity {
+public class MainActivity<ActivityMainBinding> extends Activity {
 
-   Button btnSimple;
-   Button btnAvanzado;
- private ActivityMainBinding binding;
+
+    private ActivityMainBinding binding;
+    private Button btnAvanzado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_main);
-        // btnSimple= findViewById(R.id.btnSimple);
-        // btnAvanzado= findViewById(R.id.btnAvanzado);
-        bingding = ActivityMainBinding.inflete(getLayoutInflater());
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        btnAvanzado = bingding.btnAvanzado;
+        btnAvanzado = binding.btnAvanzado;
+        // equivale a btnAvanzado = findViewById(R.id.btnAvanzado)
     }
-
-    public void btnSimpleClick(View view) {
-        Intent intent = new Intent(this, SimpleListActivity.class);
-        startActivity(intent);
-    }
-
-
 
     public void btnAvanzadoClick(View view){
-
-        Intent intent = new Intent(this, AvancedListActivity.class);
+        Intent intent = new Intent(this, AdvancedListActivity.class);
+        startActivity(intent);
     }
-
 }
+
+
+
+
+
+
+
